e0ecb003e1d3fc494d26bb9e882927cc979970bd
Davi Gonçalves <davi22morais@gmail.com>
2020-04-04 17:50:10 -0300

    feat: add phone and country information fields

--- Initial-Form/Form1.Designer.cs
+++ Initial-Form/Form1.Designer.cs
@@ -45,6 +45,10 @@
             this.lblStatus = new System.Windows.Forms.Label();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtName
@@ -114,7 +118,7 @@
             // txtPass
             // 
             this.txtPass.Font = new System.Drawing.Font("Arial", 11.25F);
-            this.txtPass.Location = new System.Drawing.Point(20, 259);
+            this.txtPass.Location = new System.Drawing.Point(20, 320);
             this.txtPass.Margin = new System.Windows.Forms.Padding(4);
             this.txtPass.Name = "txtPass";
             this.txtPass.Size = new System.Drawing.Size(202, 25);
@@ -123,7 +127,7 @@
             // txtEmail
             // 
             this.txtEmail.Font = new System.Drawing.Font("Arial", 11.25F);
-            this.txtEmail.Location = new System.Drawing.Point(20, 188);
+            this.txtEmail.Location = new System.Drawing.Point(20, 249);
             this.txtEmail.Margin = new System.Windows.Forms.Padding(4);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(202, 25);
@@ -132,7 +136,7 @@
             // txtConEmail
             // 
             this.txtConEmail.Font = new System.Drawing.Font("Arial", 11.25F);
-            this.txtConEmail.Location = new System.Drawing.Point(241, 188);
+            this.txtConEmail.Location = new System.Drawing.Point(241, 249);
             this.txtConEmail.Margin = new System.Windows.Forms.Padding(4);
             this.txtConEmail.Name = "txtConEmail";
             this.txtConEmail.Size = new System.Drawing.Size(202, 25);
@@ -141,7 +145,7 @@
             // txtConPass
             // 
             this.txtConPass.Font = new System.Drawing.Font("Arial", 11.25F);
-            this.txtConPass.Location = new System.Drawing.Point(241, 259);
+            this.txtConPass.Location = new System.Drawing.Point(241, 320);
             this.txtConPass.Margin = new System.Windows.Forms.Padding(4);
             this.txtConPass.Name = "txtConPass";
             this.txtConPass.Size = new System.Drawing.Size(202, 25);
@@ -162,7 +166,7 @@
             // 
             this.lblEmail.AutoSize = true;
             this.lblEmail.Font = new System.Drawing.Font("Arial", 11.25F);
-            this.lblEmail.Location = new System.Drawing.Point(17, 167);
+            this.lblEmail.Location = new System.Drawing.Point(17, 228);
             this.lblEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblEmail.Name = "lblEmail";
             this.lblEmail.Size = new System.Drawing.Size(50, 17);
@@ -173,7 +177,7 @@
             // 
             this.lblConEmail.AutoSize = true;
             this.lblConEmail.Font = new System.Drawing.Font("Arial", 11.25F);
-            this.lblConEmail.Location = new System.Drawing.Point(238, 167);
+            this.lblConEmail.Location = new System.Drawing.Point(238, 228);
             this.lblConEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblConEmail.Name = "lblConEmail";
             this.lblConEmail.Size = new System.Drawing.Size(106, 17);
@@ -184,7 +188,7 @@
             // 
             this.lblPass.AutoSize = true;
             this.lblPass.Font = new System.Drawing.Font("Arial", 11.25F);
-            this.lblPass.Location = new System.Drawing.Point(17, 238);
+            this.lblPass.Location = new System.Drawing.Point(17, 299);
             this.lblPass.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPass.Name = "lblPass";
             this.lblPass.Size = new System.Drawing.Size(74, 17);
@@ -195,7 +199,7 @@
             // 
             this.lblConPass.AutoSize = true;
             this.lblConPass.Font = new System.Drawing.Font("Arial", 11.25F);
-            this.lblConPass.Location = new System.Drawing.Point(238, 238);
+            this.lblConPass.Location = new System.Drawing.Point(238, 299);
             this.lblConPass.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblConPass.Name = "lblConPass";
             this.lblConPass.Size = new System.Drawing.Size(130, 17);
@@ -216,7 +220,7 @@
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(345, 308);
+            this.btnSave.Location = new System.Drawing.Point(345, 369);
             this.btnSave.Margin = new System.Windows.Forms.Padding(4);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(98, 34);
@@ -228,7 +232,7 @@
             // btnDelete
             // 
             this.btnDelete.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDelete.Location = new System.Drawing.Point(223, 308);
+            this.btnDelete.Location = new System.Drawing.Point(223, 369);
             this.btnDelete.Margin = new System.Windows.Forms.Padding(4);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(98, 34);
@@ -237,11 +241,55 @@
             this.btnDelete.UseVisualStyleBackColor = true;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Arial", 11.25F);
+            this.txtPhone.Location = new System.Drawing.Point(20, 179);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(202, 25);
+            this.txtPhone.TabIndex = 16;
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Font = new System.Drawing.Font("Arial", 11.25F);
+            this.txtCountry.Location = new System.Drawing.Point(241, 179);
+            this.txtCountry.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(202, 25);
+            this.txtCountry.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 11.25F);
+            this.label2.Location = new System.Drawing.Point(17, 158);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Phone";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 11.25F);
+            this.label1.Location = new System.Drawing.Point(238, 158);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Country";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(460, 358);
+            this.ClientSize = new System.Drawing.Size(460, 416);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.txtPhone);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.lblStatus);
@@ -288,6 +336,10 @@
         private System.Windows.Forms.Label lblStatus;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
